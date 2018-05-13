5387ddbb0c5f2f76dd311dd89f725757530353dd
root <root@localhost.localdomain>
2018-05-13 11:11:35 +0700

    twilio and nexmo for sms gateway

---
+++ 00_Percobaan/03_sms-gateway/01_nexmo/app.py
@@ -0,0 +1,8 @@
+#yang ini sudah bisa, namun untuk mengirim pesan hanya bisa ke nomor yang telah di added di akun nexmo
+import nexmo
+
+client = nexmo.Client(key='88a0f438', secret='0V8hE3SQZfVpkF9w')
+
+response = client.send_message({'from': 'Python', 'to': '6281275803651', 'text': 'Hello there, this from Nexmo..!!!'})
+
+print(response)
