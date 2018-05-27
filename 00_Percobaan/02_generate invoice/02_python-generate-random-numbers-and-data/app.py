79b5f298dbb831bf2bb0dec71ffbbcd39d8c9009
root <root@localhost.localdomain>
2018-05-27 21:50:24 +0700

    pemesan kamar sudah dapat email notifikasi pemesanan kamar, dan admin juga sudah bisa mendapat email dan sms gateway otomatis jika ada yang memesan kamar, dan menghapus file-file percobaan

--- 00_Percobaan/02_generate invoice/02_python-generate-random-numbers-and-data/app.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-def generator_random(size=10, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for x in range(size))
-
-generate = 'HR' + generator_random() + 'INV'
-
-print(generate)
