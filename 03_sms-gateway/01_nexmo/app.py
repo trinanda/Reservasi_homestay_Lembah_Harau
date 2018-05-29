ce2ceb983cd093152f0b73656a0d5f207e1e9de5
root <root@localhost.localdomain>
2018-05-30 02:07:33 +0700

    rencana selanjutnya adalah menambahkan maps ke apliaski

--- 03_sms-gateway/01_nexmo/app.py
+++
@@ -1,8 +0,0 @@
-#yang ini sudah bisa, namun untuk mengirim pesan hanya bisa ke nomor yang telah di added di akun nexmo
-import nexmo
-
-client = nexmo.Client(key='88a0f438', secret='0V8hE3SQZfVpkF9w')
-
-response = client.send_message({'from': 'Python', 'to': '62081275803651', 'text': 'Hello there, this from Nexmo..!!!'})
-
-
